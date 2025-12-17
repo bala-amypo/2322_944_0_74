0ccf967265110acf658df27b78ea2b139e012b81
bala-amypo <bala@amypo.in>
2025-12-17 08:02:34 +0000

    User commit

--- src/main/java/com/example/demo/newentity/Newentity.java
+++ src/main/java/com/example/demo/newentity/Newentity.java
@@ -1,5 +1,11 @@
 package com.example.demo.newentity;
-import jakarta.per
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 public class Newentity{
-
+    @Id
+    private Long id;
+    @NotBlank(message="should not blank")
+    private Stirng name;
+    @E
+    private String email;
 }
