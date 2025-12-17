71769aa2312f12a9f2f211579c8a56220e4ea2e6
bala-amypo <bala@amypo.in>
2025-12-17 09:22:32 +0000

    User commit

--- src/main/java/com/example/demo/newservice/newserviceimpl/Newserviceimpl.java
+++ src/main/java/com/example/demo/newservice/newserviceimpl/Newserviceimpl.java
@@ -1,7 +1,21 @@
 package com.example.demo.newservice.newserviceimpl;
 
 public class Newseviceimpl implements Newservice{
-    private final New
+    private final Newfilerepo rep;
+    
     @Override
+    public Newentity savedata(Newentity new){
+
+    }
+    @Override
+    public Newentity getidval(Long id){
+
+    }
+    @Override
+    public List<Newentity>getall(){
+
+    }
+    @Override
+
 
 }
